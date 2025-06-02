80bc658a755234fde373dda18d81cdb636356f48
Benjamin Hidalgo <benjaminhidalgo123@gmail.com>
2025-06-01 23:09:22 -0400

    Base API Reporte

---
+++ src/main/java/com/api/apireporte/ApireporteApplication.java
@@ -0,0 +1,13 @@
+package com.api.apireporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApireporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApireporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/apireporte/ApireporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.apireporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApireporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
